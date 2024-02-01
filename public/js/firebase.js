4353def16e726596c002d1753865392f049dad93
juan061734 <mateogutierrez0103@gmail.com>
2024-02-01 14:21:19 -0500

    Signingform and logincheck

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,15 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
+//import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
+    // Paste your firebase config here
 
+    apiKey: "AIzaSyBwq1iT_vULXFGgTJ0Hac0bXEFHPTNzEGM",
+    authDomain: "pau-design-709a5.firebaseapp.com",
+    projectId: "pau-design-709a5",
+    storageBucket: "pau-design-709a5.appspot.com",
+    messagingSenderId: "787739831335",
+    appId: "1:787739831335:web:02f5817ae921a3a4eeb202"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+export const db = getFirestore(app)
