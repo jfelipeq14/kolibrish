6ea832bb8ea8d9e87a685181fcbb21d11a514f0b
juan061734 <juan201pablo@gmail.com>
2024-02-05 14:52:07 -0500

    add configuration for collection

--- public/js/main.js
+++ public/js/main.js
@@ -6,70 +6,18 @@ import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.10.0/fi
 // } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js'
 import { auth, db } from './config/firebase.js' // configuracion de firebase
 import { loginCheck } from './services/auth/loginCheck.js' // funcion para validar el login
+import { setupColecciones } from './services/crud/coleccionesList.js'
+import { getDocs, collection } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
+
+// import { getData } from './services/crud/getData.js'
 
 // Rutas de los archivos a utilizar
 import './services/auth/signinForm.js'
 import './services/auth/logout.js'
 // #endregion
 
+
 // #region pruebas
-import { setupColecciones } from './services/crud/coleccionesList.js'
-const array = [
-  {
-    id: 1234,
-    nombre: 'feb-1',
-    'fecha-creacion': '2024-02-01',
-    'fecha-modificacion': '2024-02-04',
-    'fecha-finalizacion': '2025-02-04',
-    conjuntos: [
-      {
-        id: 5678,
-        nombre: 'Depor',
-        descripcion: 'Chimba pa correr',
-        habilitado: true
-      },
-      {
-        id: 6678,
-        nombre: 'Cas',
-        descripcion: 'Nada. no sé',
-        habilitado: true
-      },
-      {
-        id: 6778,
-        nombre: 'Random',
-        descripcion: 'No, mejor no lo compre',
-        habilitado: true
-      }
-    ]
-  },
-  {
-    id: 9876,
-    nombre: 'feb-2',
-    'fecha-creacion': '2024-02-01',
-    'fecha-modificacion': '2024-02-04',
-    'fecha-finalizacion': '2025-02-04',
-    conjuntos: [
-      {
-        id: 5678,
-        nombre: 'Super',
-        descripcion: 'Melo',
-        habilitado: true
-      },
-      {
-        id: 6678,
-        nombre: 'Casteo',
-        descripcion: 'A mimir',
-        habilitado: true
-      },
-      {
-        id: 6778,
-        nombre: 'Nothing',
-        descripcion: 'No,sdfdsaf compre',
-        habilitado: true
-      }
-    ]
-  }
-]
 // #endregion
 
 // estado de autenticacion, validar el usuario y cargar datos de la base de datos
@@ -77,12 +25,12 @@ onAuthStateChanged(auth, async (user) => {
   if (user) {
     loginCheck(user)
     try {
-      setupColecciones([])
+      console.log("Goleador");
     } catch (error) {
       console.log(error)
     }
   } else {
-    setupColecciones(array)
+    setupColecciones("colecciones")
     loginCheck(user)
   }
 })
