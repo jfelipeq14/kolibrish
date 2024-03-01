4bb833211bfd2a05d0c0822badb0e434ad015f26
jfelipe14 <juan_fquintero@soy.sena.edu.co>
2024-03-01 16:36:15 -0500

    add page with js but withing buttons

--- public/js/main.js
+++ public/js/main.js
@@ -8,18 +8,24 @@ import { auth } from './config/firebase.js'
 import { loginCheck } from './services/auth/loginCheck.js'
 import './services/auth/signinForm.js'
 import './services/auth/logout.js'
+import './services/index/index.js'
+
 // #endregion
 // #region functions
 // -------- TODAS LAS FUNCIONES VAN ACA SIN ESTE COMENTARIO --------
+window.addEventListener('load', () => {
+  onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      try {
+        loginCheck(user)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  })
+})
+
 // #endregion
 // #region events
-onAuthStateChanged(auth, async (user) => {
-  if (user) {
-    try {
-      loginCheck(user)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-})
+
 // #endregion
--- public/js/services/index/index.js
+++ public/js/services/index/index.js
@@ -3,11 +3,38 @@
 import { searchById } from '../general/search.js'
 const categoriasList = document.getElementById('categorias')
 const productosList = document.getElementById('productos')
-const buttons = document.getElementById('categorias')
 const listProductos = []
+const buttons = document.getElementById('categorias')
 let dataCategory
 // #endregion
 // #region functions
+window.addEventListener('load', () => {
+  const content = document.getElementById('content')
+  const wrapper = document.createElement('article')
+  // add page
+  wrapper.innerHTML = [
+    '<section id="main" class="d-flex">',
+    '<article>',
+    '<h1 class="title">NUEVO PRODUCTO</h1>',
+    '<button class="btn-white btn-sm">Categoria</button>',
+    '</article>',
+    '<article>',
+        `<a href="#categorias" class="btn-sm">
+        <img
+          src="https://images.wallpaperscraft.com/image/single/astronaut_spacesuit_helmet_1185318_240x320.jpg"
+          alt="Descripcion"
+        />
+      </a>`,
+        '</article>',
+        '</section>',
+        '<section class="section">',
+        '<header id="categorias"></header>',
+        '<article id="productos"></article>',
+        '</section>'
+  ].join('')
+  content.append(wrapper)
+})
+
 export const addButtons = async (data) => {
   if (data.length) {
     data.forEach((data) => {
